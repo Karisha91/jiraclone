71dbce9e04a85cc9d047162c105a3f2f1bfdf677
Karisha91 <ivan.djurdjevic991@gmail.com>
2026-03-29 13:40:02 +0200

    Add Vercel URL to CORS config

--- src/main/java/com/ivan/jiraclone/CorsConfig.java
+++ src/main/java/com/ivan/jiraclone/CorsConfig.java
@@ -14,7 +14,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://jiraclone-frontend.vercel.app")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://jiraclone-frontend.vercel.app",
+                                "https://jiraclone-frontend-karisha91s-projects.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
