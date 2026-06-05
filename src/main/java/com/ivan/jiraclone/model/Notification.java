700bc74b6ebc874df29bc164f6d337408740fbf6
Karisha91 <ivan.djurdjevic991@gmail.com>
2026-06-05 13:28:43 +0200

    feat: add Notification entity, repository, persist notifications to DB, add GET and mark as read endpoints

---
+++ src/main/java/com/ivan/jiraclone/model/Notification.java
@@ -0,0 +1,35 @@
+package com.ivan.jiraclone.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "notifications")
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(length = 2000)
+    private String message;
+
+    @ManyToOne
+    @JoinColumn(name = "issue_id")
+    private Issue issue;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    private boolean isRead;
+
+    private LocalDateTime createdAt;
+
+}
--- src/main/java/com/ivan/jiraclone/service/NotificationService.java
+++ src/main/java/com/ivan/jiraclone/service/NotificationService.java
@@ -1,27 +1,76 @@
 package com.ivan.jiraclone.service;
 
 
+import com.ivan.jiraclone.Repository.IssueRepository;
+import com.ivan.jiraclone.Repository.NotificationRepository;
+import com.ivan.jiraclone.Repository.UserRepository;
 import com.ivan.jiraclone.dto.NotificationDTO;
+import com.ivan.jiraclone.model.Notification;
+import com.ivan.jiraclone.model.User;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Service
 public class NotificationService {
 
     private final SimpMessagingTemplate messagingTemplate;
+    private final NotificationRepository notificationRepository;
+    private final UserRepository userRepository;
+    private final IssueRepository issueRepository;
 
 
-    public NotificationService(SimpMessagingTemplate messagingTemplate) {
+
+    public NotificationService(SimpMessagingTemplate messagingTemplate, NotificationRepository notificationRepository, UserRepository userRepository, IssueRepository issueRepository) {
         this.messagingTemplate = messagingTemplate;
+        this.notificationRepository = notificationRepository;
+        this.userRepository = userRepository;
+        this.issueRepository = issueRepository;
     }
 
 
     public void sendNotification(Long userId, String message, Long issueId) {
-        NotificationDTO notification = new NotificationDTO(message, issueId);
+        Notification notification  = saveNotification(userId, message, issueId);
+        NotificationDTO notificationDTO = new NotificationDTO(message, issueId, notification.getId(), notification.isRead());
+
         messagingTemplate.convertAndSend(
                 "/queue/notifications/" + userId,
-                notification
+                notificationDTO
         );
     }
 
+    public Notification saveNotification(Long userId, String message, Long issueId) {
+        Notification notification = new Notification();
+        notification.setMessage(message);
+        notification.setIssue(issueRepository.findById(issueId).orElseThrow(() -> new RuntimeException("Issue not found")));
+        notification.setUser(userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found")));
+        notification.setRead(false);
+        notification.setCreatedAt(java.time.LocalDateTime.now());
+        return notificationRepository.save(notification);
+
+    }
+
+    public List<NotificationDTO> getNotificationsForCurrentUser(Long userId) {
+        return convertToDto(notificationRepository.findByUserId(userId));
+
+    }
+
+    public List<NotificationDTO> convertToDto(List<Notification> notifications) {
+        List<NotificationDTO> dtos = new ArrayList<>();
+        for (Notification notification : notifications) {
+            dtos.add(new NotificationDTO(notification.getMessage(), notification.getIssue().getId(), notification.getId(), notification.isRead()));
+        }
+        return dtos;
+    }
+
+    public NotificationDTO markAsRead(Long id) {
+        Notification notification = notificationRepository.findById(id).orElseThrow(() -> new RuntimeException("Notification not found"));
+        notification.setRead(true);
+        notificationRepository.save(notification);
+        return new NotificationDTO(notification.getMessage(), notification.getIssue().getId(), notification.getId(), notification.isRead());
+    }
 }
+
